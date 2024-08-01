d469b088c92ce2fc369f06d0cae5b3f0faf837db
Rama-odeh <ramaabdullah573@gmail.com>
2024-08-01 15:56:16 +0300

    update

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -1,5 +1,6 @@
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,6 +16,9 @@ public class MyTestCases {
 	WebDriver driver = new ChromeDriver();
 	Actions actions = new Actions(driver);
 	String url = "https://magento.softwaretestingboard.com/";
+	Random rand = new Random();
+	double numbers = 0;
+	double expectedPrice = 70.0;
 
 	@BeforeTest
 	public void mySetup() {
@@ -90,7 +94,76 @@ public class MyTestCases {
 			driver.navigate().back();
 
 			products = driver.findElements(By.cssSelector(".item.product.product-item"));
-
+			Thread.sleep(2000);
 		}
 	}
+
+	@Test(priority = 4)
+	public void theTotalItemsInTheCart() throws InterruptedException {
+		WebElement itemsNumberOnTheCart = driver.findElement(By.className("counter-number"));
+		String itemsNumberOnTheCartAsString = itemsNumberOnTheCart.getText();
+		int itemsNumberOnTheCartAsInt = Integer.parseInt(itemsNumberOnTheCartAsString);
+		Assert.assertEquals(itemsNumberOnTheCartAsInt, 3);
+
+		driver.findElement(By.cssSelector(".action.showcart")).click();
+		WebElement itemss = driver.findElement(By.className("minicart-items-wrapper"));
+
+		List<WebElement> items = itemss.findElements(By.cssSelector(".item.product.product-item"));
+		WebElement item;
+		for (int i = 0; i < items.size(); i++) {
+			item = items.get(i);
+			WebElement prices = item.findElement(By.className("price"));
+			String price = prices.getText().replace("$", "");
+			double num = Double.parseDouble(price);
+			numbers += num;
+
+		}
+
+		Assert.assertEquals(numbers, expectedPrice);
+		Thread.sleep(2000);
+	}
+
+	@Test(priority = 5)
+	public void totalPriceRandomly() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.findElement(By.id("btn-minicart-close")).click();
+		Thread.sleep(4000);
+		List<WebElement> items = driver.findElements(By.cssSelector(".item.product.product-item"));
+		int randIndex = rand.nextInt(12);
+		WebElement item = items.get(randIndex);
+		WebElement productLink = item.findElement(By.cssSelector(".product-item-link"));
+		productLink.click();
+
+		WebElement sizeElement = driver.findElement(By.id("option-label-size-143-item-166"));
+		sizeElement.click();
+
+		List<WebElement> colorElements = driver.findElements(By.cssSelector(".swatch-option.color"));
+		for (WebElement colorElement : colorElements) {
+			if (colorElement.getAttribute("aria-label").equals("Black")
+					|| colorElement.getAttribute("aria-label").equals("Blue")
+					|| colorElement.getAttribute("aria-label").equals("Green")
+					|| colorElement.getAttribute("aria-label").equals("Red")) {
+				colorElement.click();
+				break;
+			}
+
+		}
+
+		WebElement addToCartButton = driver.findElement(By.cssSelector(".action.tocart.primary"));
+		addToCartButton.click();
+
+		driver.navigate().back();
+		Thread.sleep(3000);
+
+		driver.findElement(By.cssSelector(".action.showcart")).click();
+		Thread.sleep(2000);
+		WebElement prices = driver.findElement(By.className("price"));
+		String price = prices.getText().replace("$", "");
+		double num = Double.parseDouble(price);
+		numbers += num;
+		System.out.println(numbers);
+		System.out.println(expectedPrice);
+		Assert.assertEquals(numbers > expectedPrice, true);
+	}
+
 }
